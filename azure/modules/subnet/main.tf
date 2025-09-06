9b4793e2b2bd6aee3cea6cb910d88e96f78bda3b
Shashank Palla <shashankpalla44@gmail.com>
2025-09-06 23:37:09 +0530

    updated vnet name in subnet main.tf

--- azure/modules/subnet/main.tf
+++ azure/modules/subnet/main.tf
@@ -2,6 +2,6 @@ resource "azurerm_subnet" "robot_shop_subnet" {
     for_each                = { for subnet in var.subnet: subnet.name => subnet }
     name                 = each.value.name
     resource_group_name  = each.value.resource_group
-    virtual_network_name = azurerm_virtual_network.robot_shop_subnet[each.value.vnet_name].name
+    virtual_network_name = azurerm_virtual_network.robot_shop_vnet[each.value.vnet_name].name
     address_prefixes     = each.value.address_prefixes 
 }
