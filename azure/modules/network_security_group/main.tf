880f3cc7b12a543a14f9ac94ff3b4621ffc1ad60
Shashank Palla <shashankpalla44@gmail.com>
2025-09-11 15:59:04 +0530

    updated rule referencing key

--- azure/modules/network_security_group/main.tf
+++ azure/modules/network_security_group/main.tf
@@ -8,17 +8,17 @@ resource "azurerm_network_security_group" "robot_shop_nsg" {
 }
 
 resource "azurerm_network_security_rule" "robot_shop_nsg_rule" {
-    for_each = { for rule in local.nsg_rules : rule.key => rule.rule }
+    for_each = { for rule in local.nsg_rules : rule.key => rule }
 
-    name                        = each.value.name
-    priority                    = each.value.priority
-    direction                   = each.value.direction
-    access                      = each.value.access
-    protocol                    = each.value.protocol
-    source_port_range           = each.value.source_port_range
-    destination_port_range      = each.value.destination_port_range
-    source_address_prefix       = each.value.source_address_prefix
-    destination_address_prefix  = each.value.destination_address_prefix
+    name                        = each.value.rule.name
+    priority                    = each.value.rule.priority
+    direction                   = each.value.rule.direction
+    access                      = each.value.rule.access
+    protocol                    = each.value.rule.protocol
+    source_port_range           = each.value.rule.source_port_range
+    destination_port_range      = each.value.rule.destination_port_range
+    source_address_prefix       = each.value.rule.source_address_prefix
+    destination_address_prefix  = each.value.rule.destination_address_prefix
     resource_group_name         = each.value.resource_group_name
     network_security_group_name = azurerm_network_security_group.robot_shop_nsg[each.value.nsg_name].name
 }
