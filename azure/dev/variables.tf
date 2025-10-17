f0d92483a5e497ee363f169fe9ecd9a99fcb4cfc
Shashank Palla <shashankpalla44@gmail.com>
2025-10-17 10:11:47 +0530

    commented load balancer references

--- azure/dev/variables.tf
+++ azure/dev/variables.tf
@@ -181,24 +181,24 @@ variable "kubernetes_nodes" {
     }))
 }
 
-variable "load_balancer" {
-    type = list(object({
-        name                = string
-        resource_group_name = string
-        location            = string
-        sku                 = string
-        tags                = map(string)
+# variable "load_balancer" {
+#     type = list(object({
+#         name                = string
+#         resource_group_name = string
+#         location            = string
+#         sku                 = string
+#         tags                = map(string)
 
-        frontend_ip_configuration = object({
-            name                 = string
-            public_ip_address_id = string
-        })
-    }))
-}
+#         frontend_ip_configuration = object({
+#             name                 = string
+#             public_ip_address_id = string
+#         })
+#     }))
+# }
 
-variable "backend_address_pool" {
-    type = list(object({
-        name         = string
-        loadbalancer = string
-    }))
-}
+# variable "backend_address_pool" {
+#     type = list(object({
+#         name         = string
+#         loadbalancer = string
+#     }))
+# }
