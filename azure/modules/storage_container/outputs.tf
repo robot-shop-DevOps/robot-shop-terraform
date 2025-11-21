6311923196a0de2f145b2a54a0a2ba1d2eba5a0e
Shashank Palla <pallasai_shashank@epam.com>
2025-11-21 12:50:10 +0530

    updated provider version

--- azure/modules/storage_container/outputs.tf
+++ azure/modules/storage_container/outputs.tf
@@ -1,4 +1,4 @@
 output "storage_container_id" {
     description = "Map of Storage Container IDs keyed by Storage Container name"
-    value       = { for sc in azurerm_storage_container.DevOps_storage_container : sc.name => sc.id }
+    value       = { for sc in azurerm_storage_container.storage_container : sc.name => sc.id }
 }
