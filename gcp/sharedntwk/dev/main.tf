2524a1cbcb3b70487a418f55e17c0770fb1c915d
ShashankPalla2002 <shashankpalla44@gmail.com>
2026-09-17 20:38:10 +0530

    feat(sharedntwk): added terraform to create subnetworks

--- gcp/sharedntwk/dev/main.tf
+++ gcp/sharedntwk/dev/main.tf
@@ -7,3 +7,9 @@ module "vpc" {
   source = "../../modules/vpc"
   vpc_network = var.vpc_network
 }
+
+module "subnetwork" {
+  source = "../../modules/subnetwork"
+  subnetwork_ipv4 = var.subnetwork-ipv4
+  vpc_network = module.vpc.vpc_network_id
+}
