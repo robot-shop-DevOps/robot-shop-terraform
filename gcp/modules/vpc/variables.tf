049e207107cdbc67e464362f325b04eee227554a
ShashankPalla2002 <shashankpalla44@gmail.com>
2026-09-17 20:12:19 +0530

    feat(gcp-modules): vpc: added deletion_policy variable

--- gcp/modules/vpc/variables.tf
+++ gcp/modules/vpc/variables.tf
@@ -5,6 +5,7 @@ variable "vpc_network" {
         description             = optional(string)
         auto_create_subnetworks = optional(bool)
         routing_mode            = optional(string)
+        deletion_policy         = optional(string, "PREVENT")
     }))
     default = []
 }
