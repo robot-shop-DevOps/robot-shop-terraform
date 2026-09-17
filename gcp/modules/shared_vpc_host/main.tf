26afa6c434d272633271d690ab97ceb2dbdfb8e0
ShashankPalla2002 <shashankpalla44@gmail.com>
2026-09-17 19:27:24 +0530

    fix(sharedntwk): passing variable to shared vpc host module

--- gcp/modules/shared_vpc_host/main.tf
+++ gcp/modules/shared_vpc_host/main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_shared_vpc_host_project" "shared_vpc_host" {
-    for_each = {for vpc in var.shared_vpc_host : vpc.project => vpc}
+    for_each = {for vpc in var.shared_vpc_host : vpc.name => vpc}
 
     project = each.value.project
 }
--- gcp/sharedntwk/dev/main.tf
+++ gcp/sharedntwk/dev/main.tf
@@ -1,7 +1,4 @@
 module "shared_vpc_host" {
   source = "../../modules/shared_vpc_host"
-}
-
-module "shared_vpc_service" {
-  source = "../../modules/shared_vpc_service"
+  shared_vpc_host = var.shared_vpc_host
 }
