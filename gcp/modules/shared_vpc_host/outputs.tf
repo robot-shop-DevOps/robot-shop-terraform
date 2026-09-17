09297d674b0791684a77f90b9d73b9358eaea06a
ShashankPalla2002 <shashankpalla44@gmail.com>
2026-09-17 19:37:42 +0530

    feat(gcp-modules): shared_vpc_host: removed name variable, added deletion_policy variable

--- gcp/modules/shared_vpc_host/outputs.tf
+++ gcp/modules/shared_vpc_host/outputs.tf
@@ -1,4 +1,4 @@
 output "shared_vpc_host_id" {
-    description = "Map of Shared VPC Host IDs keyed by name"
-    value       = { for vpc in google_compute_shared_vpc_host_project.shared_vpc_host : vpc.name => vpc.id}
+    description = "Map of Shared VPC Host IDs keyed by project"
+    value       = { for vpc in google_compute_shared_vpc_host_project.shared_vpc_host : vpc.project => vpc.id}
 }
