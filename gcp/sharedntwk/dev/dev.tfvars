c90c6f69f667b414366a9c89fc32f43c24d74256
ShashankPalla2002 <shashankpalla44@gmail.com>
2026-09-17 19:38:31 +0530

    fix(gcp-sharedntw): updated variables and tfvars

--- gcp/sharedntwk/dev/dev.tfvars
+++ gcp/sharedntwk/dev/dev.tfvars
@@ -1,6 +1,5 @@
 shared_vpc_host = [
     {
-        name    = "RobotShop-Shared-VPC-Host",
         project = "robotshop-sharedntwk-dev"
     }
 ]
--- gcp/sharedntwk/dev/variables.tf
+++ gcp/sharedntwk/dev/variables.tf
@@ -1,7 +1,7 @@
 variable "shared_vpc_host" {
     type = list(object({
-        name    = string
-        project = string
+        project         = string
+        deletion_policy = optional(string, "PREVENT")
     }))
     default = []
 }
