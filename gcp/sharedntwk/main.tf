eab2ef79d5d5ede97fbcc2d60889daeedf49d2a4
ShashankPalla2002 <shashankpalla44@gmail.com>
2026-09-22 22:13:28 +0530

    fix(gcp-sharedntwk): updated source path for modules

--- gcp/sharedntwk/main.tf
+++ gcp/sharedntwk/main.tf
@@ -1,26 +1,26 @@
 module "shared_vpc_host" {
-  source = "../../modules/shared_vpc_host"
+  source = "../modules/shared_vpc_host"
   shared_vpc_host = var.shared_vpc_host
 }
 
 module "shared_vpc_service" {
-  source = "../../modules/shared_vpc_service"
+  source = "../modules/shared_vpc_service"
   shared_vpc_service = var.shared_vpc_service
 }
 
 module "vpc" {
-  source = "../../modules/vpc"
+  source = "../modules/vpc"
   vpc_network = var.vpc_network
 }
 
 module "subnetworks" {
-  source          = "../../modules/subnetworks"
+  source          = "../modules/subnetworks"
   subnetwork-ipv4 = var.subnetwork-ipv4
   vpc_network     = module.vpc.vpc_network_id
 }
 
 module "nat" {
-  source = "../../modules/compute_instance"
+  source = "../modules/compute_instance"
 
   machine_type            = var.nat_machine_type
   name                    = var.nat_name
